8b12ac313ebcd36be67d4c8a06cb5165209fb817
Mohammad YousefiPour <moyousefipour79@gmail.com>
2025-07-21 17:25:08 +0330

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ import visualtorch
 
 model = resnet18()
 visualtorch.layered_view(
-    model, to_file='resnet-18.png', min_z=3, sizing_mode='logarithmic', legend=True)
+    model, to_file='resnet-18.tex', min_z=1,max_z=15, sizing_mode='logarithmic', legend=True,spacing=5,padding=0)
